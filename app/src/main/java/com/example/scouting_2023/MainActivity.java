1a5c4636cf18710b9267e5bce5df5692baa50a6a
Bennett11123 <118493792+Bennett11123@users.noreply.github.com>
2023-01-26 20:27:21 -0600

    Updated Auto and aligned elements along with button names.

--- app/src/main/java/com/example/scouting_2023/MainActivity.java
+++ app/src/main/java/com/example/scouting_2023/MainActivity.java
@@ -47,13 +47,18 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+                //Todo Reveiw and fix line or else...
+         //     viewPager.setCurrentItem(tab.getPosition());
             }
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
             }
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
+            }
+
+            public void incrementUpperCone(){
+
             }
         });
     }
