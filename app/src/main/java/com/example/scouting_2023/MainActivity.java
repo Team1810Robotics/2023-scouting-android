33dd64d7a07838f2826ffef17165c760618d3f51
2tanza <115197125+2tanza@users.noreply.github.com>
2023-02-04 09:45:24 -0600

    test commit

--- app/src/main/java/com/example/scouting_2023/MainActivity.java
+++ app/src/main/java/com/example/scouting_2023/MainActivity.java
@@ -48,53 +48,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void FinalSave(View view) {
 
-
-    }
-
-
-
-/*
-public class MainActivity extends AppCompatActivity {
-
-  //  TabLayout tabLayout;
-  //  ViewPager viewPager;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tabLayout = findViewById(R.id.tabLayout);
-        viewPager = findViewById(R.id.viewPager);
-        tabLayout.addTab(tabLayout.newTab().setText("Football"));
-        tabLayout.addTab(tabLayout.newTab().setText("Cricket"));
-        tabLayout.addTab(tabLayout.newTab().setText("NBA"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(),
-                tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
+        Bundle bundle = getIntent().getExtras();
+        if (bundle == null) {
+            bundle = new Bundle();
+        }
+//test comment
+      //  final DataModel data = new DataModel();
     }
 }
 
- */
- /*      fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-   */
